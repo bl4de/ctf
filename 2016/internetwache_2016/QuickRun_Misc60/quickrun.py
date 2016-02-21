d03c577d2f0a8c57a966cfc105d91437cd019370
bloorq@gmail.com <bloorq@gmail.com>
2016-02-21 14:29:35 +0000

    internetwache 2016 Quick Run changes

--- 2016/internetwache_2016/QuickRun_Misc60/quickrun.py
+++ 2016/internetwache_2016/QuickRun_Misc60/quickrun.py
@@ -3,21 +3,20 @@ import base64
 
 f = open("README.txt").read().split("\n")
 qrcode = ""
-fn = "qrcode_"
 tmp = ""
 i = 1
+
+fout = open("qrcodes.txt", "a+")
 
 for p in f:
     tmp += p
     if len(p) < 77 and p.endswith(("ilogK","==","Ao=")):
         qrcode = base64.b64decode(tmp)
-        filename = fn + str(i) + ".txt"
-        print "[+] save QRcode file " + filename
-        f = open(filename, "w")
-        f.write(qrcode)
-        f.close()
+        print "[+] save QRcode to file"
+        fout.write(qrcode + "\n\n\n")
         tmp = ""
-        i += 1
+        
+fout.close()
 
 print "[+] end"
 

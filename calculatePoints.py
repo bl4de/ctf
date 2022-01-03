48a66742d770107c95c94b9dff6293a09728fa83
Rafal Janicki <bloorq@gmail.com>
2022-01-03 09:45:36 +0000

    removed unused notes.md; calculatePoints.py refactoring to Python 3

--- calculatePoints.py
+++ calculatePoints.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # @author bl4de https://github.com/bl4de | https://ctftime.org/team/12769
 #
 # CTF event points -> CTFtime.org ranking points calculator
@@ -7,6 +7,7 @@
 #
 # Calculates team's points from CTF for CTFtime.org overall ranking
 #
+from math import floor
 
 
 def calculate():
@@ -16,17 +17,22 @@ def calculate():
     place_coef = float(1) / team_place
 
     if points_coef > 0:
-        rating = ((points_coef + place_coef) * weight) / (1/(1+(team_place/total_teams)))
+        rating = ((points_coef + place_coef) * weight) / \
+            (1/(1+(team_place/total_teams)))
 
     return rating
 
+
 if __name__ == '__main__':
 
-    team_place = int(raw_input("your team place in CTF: "))
-    team_points = int(raw_input("earned CTF points: "))
-    total_teams = int(raw_input("teams with any points (more than 0) in CTF: "))
-    best_points = int(raw_input("winner team points: "))
-    weight = int(raw_input("CTF event rating weight (by CTFtime.org): "))
+    team_place = int(input("your team place in CTF: "))
+    team_points = int(input("earned CTF points: "))
+    total_teams = int(
+        input("teams with any points (more than 0) in CTF: "))
+    best_points = int(input("winner team points: "))
+    weight = float(input("CTF event rating weight (by CTFtime.org): "))
 
-    print "\n### Your team earned {0:.3f} points in this CTF, congrats! ###"\
-        .format(calculate())
+    weight = floor(weight)
+
+    print("\nYour team earned {0:.3f} points in this CTF, congrats!".format(
+        calculate()))

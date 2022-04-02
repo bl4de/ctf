c34878c5bf9984c9dc6f02595a35cef9f76abc1f
Rafal Janicki <bloorq@gmail.com>
2022-04-03 00:38:40 +0200

    [picoGym] gdbme - solution

--- picoGym/_gdbme/gdbme.c
+++ picoGym/_gdbme/gdbme.c
@@ -32,23 +32,21 @@ int main(void)
 {
     char* __s;
     long in_FS_OFFSET;
-    int local_38;
-    int local_30;
-    int local_28;
-    int local_20;
-    int local_18;
+    long local_38;
+    long local_30;
+    long local_28;
+    long local_20;
+    long local_18;
     long local_10;
 
-    local_10 = *(long*)(in_FS_OFFSET + 0x28);
-    local_38 = 0x4c75257240343a41;
-    local_30 = 0x4362383846336235;
-    local_28 = 0x6630624760433530;
-    local_20 = 0x4e64646267353361;
+    local_10 = in_FS_OFFSET + 0x28;
+    local_20 = 0x4c75257240343a41;
+    local_28 = 0x4362383846336235;
+    local_30 = 0x6630624760433530;
+    local_38 = 0x4e64646267353361;
     local_18 = 0;
-    sleep(100000);
-    __s = (char*)rotate_encrypt(0, &local_38);
-    fputs(__s, stdout);
+    __s = (char*)rotate_encrypt(0, &local_20);
+    fputs(__s, stdout); // picoCTF{d3bugg3r_dr1v3_72bd8355}
     putchar(10);
-
     return 0;
 }
